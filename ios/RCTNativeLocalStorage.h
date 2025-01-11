b6f8f037112df128d68e7382423bc6a00511ff13
nobletp001 <ojotemitopeidnoble10@gmail.com>
2025-01-11 16:43:36 +0100

    first commit

---
+++ ios/RCTNativeLocalStorage.h
@@ -0,0 +1,16 @@
+//
+//  RCTNativeLocalStorage.h
+//  nativeTemplate
+//
+//  Created by machine on 1/11/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
